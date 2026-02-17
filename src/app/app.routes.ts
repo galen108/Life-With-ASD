a5f20e585f3f0637d545c2bf33a390797466d4fb
Galen Tenney <tenney.galen@gmail.com>
2026-02-17 00:17:21 -0500

    2/16/2026 life-with-asd origin main commit # 9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ExperienceSummaryComponent} from "./experience-summary/experience-summary.component";
+import {LifeAreasComponent} from "./life-areas/life-areas.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'experience-summary', component: ExperienceSummaryComponent},
+{path: 'life-areas', component: LifeAreasComponent}];
