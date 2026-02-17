25298f6a4fa60eae47cfd9bb0bb801d00edcac1a
Galen Tenney <tenney.galen@gmail.com>
2026-02-17 01:47:46 -0500

    2/17/2026 life-with-asd origin main commit # 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatTabNavPanel } from "@angular/material/tabs";
+import { MatTabNav, MatTabLink, MatTabNavPanel } from "@angular/material/tabs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatTabNavPanel, RouterOutlet],
+  imports: [ MatTabNav, MatTabLink, MatTabNavPanel, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'life-with-asd';
+  protected links: any;
+  protected activeLink: any;
 }
