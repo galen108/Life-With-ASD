d0111da64a0da6c14c9c5e902e337f8230e25c79
Galen Tenney <tenney.galen@gmail.com>
2026-02-17 01:41:01 -0500

    2/17/2026 life-with-asd origin main commit # 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatTabNav, MatTabLink, MatTabNavPanel } from "@angular/material/tabs";
+import { MatTabNavPanel } from "@angular/material/tabs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTabNav, MatTabLink, MatTabNavPanel],
+  imports: [MatTabNavPanel, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
